6768bb4501bd33926895fd8779ea2ee35c41a844
Sh123-max <shreekar.cd22@bmsce.ac.in>
2025-07-14 00:53:51 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,7 @@ columns = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin'
            'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome']
 df = pd.read_csv("https://raw.githubusercontent.com/madhav481010/Diabetes-Prediction/main/diabetes.csv", names=columns, header=0)
 print(df)
+print('Diabetes-preprocess')
 # Replace biologically implausible zeroes with NaN
 na_columns = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
 df[na_columns] = df[na_columns].replace(0, np.nan)
